621f7404ebd435784cb12a81049c37cd94890cf0
wesly <weslyambarita4@gmail.com>
2026-04-19 14:42:36 +0700

    update

--- app/models/football.py
+++ app/models/football.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, Text, String, DateTime, ForeignKey # <-- Tambahkan String di sini
 from datetime import datetime, timezone
 from app.extensions import Base
 
 class Football(Base):
-    __tablename__ = "football_clubs" # Ubah nama tabel
+    __tablename__ = "football_clubs"
 
     id = Column(Integer, primary_key=True)
-    name = Column(Text) # Sebelumnya 'text'
-    league = Column(String(100)) # Tambahkan kolom liga
+    name = Column(Text) 
+    league = Column(String(100)) # Sekarang String sudah dikenali
     request_id = Column(Integer, ForeignKey("requests.id"))
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
